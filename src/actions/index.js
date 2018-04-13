13fdb354e83ddb90194f653cbe63528b28985e5c
Chris Walsh <cwalsh@leapfrogonline.com>
2018-04-13 16:15:55 -0500

    calls the api for that forecast

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const WEATHER_API_KEY = 'ebb9b7e26af8aa27a341fb0855594406';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
